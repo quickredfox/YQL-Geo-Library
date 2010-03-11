52383a36353b468eb44fdc6beaf2215c4806668c
neyric <eric.abouaf@gmail.com>
2010-03-11 18:52:41 +0100

    Find the user using the Gears API (works for Chrome)

--- yqlgeo.js
+++ yqlgeo.js
@@ -5,6 +5,61 @@
   Code licensed under the BSD License:
   http://wait-till-i.com/license.txt
 */
+
+// -- BEGIN GEARS_INIT
+(function() {
+  // We are already defined. Hooray!
+  if (window.google && google.gears) {
+    return;
+  }
+
+  var factory = null;
+
+  // Firefox
+  if (typeof GearsFactory != 'undefined') {
+    factory = new GearsFactory();
+  } else {
+    // IE
+    try {
+      factory = new ActiveXObject('Gears.Factory');
+      // privateSetGlobalObject is only required and supported on WinCE.
+      if (factory.getBuildInfo().indexOf('ie_mobile') != -1) {
+        factory.privateSetGlobalObject(this);
+      }
+    } catch (e) {
+      // Safari
+      if ((typeof navigator.mimeTypes != 'undefined') && navigator.mimeTypes["application/x-googlegears"]) {
+        factory = document.createElement("object");
+        factory.style.display = "none";
+        factory.width = 0;
+        factory.height = 0;
+        factory.type = "application/x-googlegears";
+        document.documentElement.appendChild(factory);
+      }
+    }
+  }
+
+  // *Do not* define any objects if Gears is not installed. This mimics the
+  // behavior of Gears defining the objects in the future.
+  if (!factory) {
+    return;
+  }
+
+  // Now set up the objects, being careful not to overwrite anything.
+  //
+  // Note: In Internet Explorer for Windows Mobile, you can't add properties to
+  // the window object. However, global objects are automatically added as
+  // properties of the window object in all browsers.
+  if (!window.google) {
+    google = {};
+  }
+
+  if (!google.gears) {
+    google.gears = {factory: factory};
+  }
+})();
+// -- END GEARS_INIT
+
 var yqlgeo = function(){
   var callback;
   function get(){
@@ -50,6 +105,8 @@ var yqlgeo = function(){
           retrieveip();
         }
       );
+	 } else if(window.google && google.gears) {
+		retrieveGears();
     } else{
       retrieveip();
     }
@@ -65,6 +122,16 @@ var yqlgeo = function(){
 
   function retrieveip(){
     jsonp('http://jsonip.appspot.com/?callback=yqlgeo.ipin');
+  };
+
+  function retrieveGears() {
+    try {
+      var geo = google.gears.factory.create('beta.geolocation');
+      geo.getCurrentPosition(function(position) { getFromLatLon(position.latitude,position.longitude); }, function() {retrieveip();});
+    }
+    catch(ex) {
+      retrieveip();
+    }
   };
 
   function ipin(o){
